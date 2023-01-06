34d84e289f1ce84a3469cd9c218c73daba4c59c7
guangbo111010528 <jan0984327881@gmail.com>
2023-01-06 16:51:05 +0800

    finish

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,53 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+//a = R0
+//b = R1
+//R2 = 0
+//while(a > 0){
+//R2 = R2 + b
+//a --
+//}
+    @R0
+    D=M
+    @a
+    M=D
+
+    @R1
+    D=M
+    @b
+    M=D
+
+    @0
+    D=A
+    @R2
+    M=D
+
+(LOOP)
+// begin of loop condition
+    @a
+    D=M
+    @END
+    D;JLE
+// begin body of while
+    @b
+    D=M
+    @R2
+    M=M-D
+// end body of while
+
+    @LOOP
+    0;JMP
+
+(END)
+    0;JMP
+
+
+
+
+
+
+
+
+
+
